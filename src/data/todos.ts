bdc0371583626c63aef2018d342a2464947000bc
Kaizen <125158784+kaizen011@users.noreply.github.com>
2024-03-23 10:21:54 +0100

    Lucide + Types w TS + basic Tailwind config

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todos";
+
+export const dummyData : Todo[] = [
+    {
+        id:1,
+        title: "Like this project 👍",
+        completed: false,
+    },
+    {
+        id:2,
+        title: "Follow me on GitHub 🧑‍💻",
+        completed: false,
+    },
+    {
+        id:3,
+        title: "Leave a nice comment 😁",
+        completed: false,
+    },
+];

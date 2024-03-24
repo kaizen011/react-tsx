2ab1655f65af773e060c8afb691ce9514e549f7f
Kaizen <125158784+kaizen011@users.noreply.github.com>
2024-03-24 20:12:49 +0100

    Twisted some tailwind styling

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ function App() {
 
   return (
     <main className="py-8 h-screen space-y-5">
-      <h1 className="font-bold text-3xl text-center">Your todos</h1>
-      <div className="max-w-lg mx-auto bg-slate-100 rounded p-5">
+      <h1 className="font-bold text-3xl text-center text-slate-300">Your todos</h1>
+      <div className="max-w-lg mx-auto bg-slate-900 rounded p-5">
         <div className="space-y-2">
           {todos.map(todo => (
             <TodoItem
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -9,14 +9,14 @@ interface TodoItemProps {
 export default function TodoItem({todoExtract, onCompletedChange, id}: TodoItemProps){
     return(
         <div>
-            <label className="flex items-center gap-2 border rounded-md p-2 border-gray-400 bg-white hover:bg-slate-200">
+            <label className="flex items-center gap-3 border-3 rounded-md p-2 border-gray-400 bg-slate-400 hover:bg-slate-200">
                 <input 
                 type="checkbox"
                 checked={todoExtract.completed}
                 onChange={(event) => onCompletedChange(todoExtract.id, event.target.checked)}
                 className="scale-125"
                 /> 
-                <span className={todoExtract.completed ? "line-through text-gray-400" : ""}>
+                <span className={todoExtract.completed ? "line-through text-gray-600" : ""}>
                     {todoExtract.title} + {id}
                 </span>
             </label>

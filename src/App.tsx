5234ca69e93d878c86c9424da2cb217c044f2593
Kaizen <125158784+kaizen011@users.noreply.github.com>
2024-03-25 21:54:46 +0100

    Added form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import TodoItem from "./components/TodoItem"
 import { dummyData } from "./data/todos"
+import AddTodoForm from "./components/AddTodoForm";
 
 function App() {
   const [todos, setTodos] = useState(dummyData);
@@ -12,10 +13,23 @@ function App() {
     );
   }
 
+  function addTodo(title:string) {
+    setTodos((prevTodos) => [
+      {
+        id: prevTodos.length + 1,
+        title,
+        completed:false
+      },
+      ...prevTodos
+    ]);
+  }
   return (
     <main className="py-8 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center text-slate-300">Your todos</h1>
-      <div className="max-w-lg mx-auto bg-slate-900 rounded p-5">
+      <div className="max-w-lg mx-auto bg-slate-900 rounded p-5 space-y-6">
+       <AddTodoForm 
+       onSubmit={addTodo}
+       />
         <div className="space-y-2">
           {todos.map(todo => (
             <TodoItem
---
+++ src/components/AddTodoForm.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+interface AddTodoFormProps {
+    onSubmit : (title: string) => void;
+}
+
+export default function AddTodoForm({onSubmit} : AddTodoFormProps){
+    const [input, setInput] = useState("")
+
+    function handleSubmit(e:React.FormEvent<HTMLFormElement>){
+        e.preventDefault();
+
+        if(!input.trim()) return;
+
+        onSubmit(input);
+        setInput("");
+    }
+
+    return(
+        <form className="flex" onSubmit={handleSubmit}>
+            <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)} 
+            placeholder="What needs to be done?"
+            className="rounded-s-md grow border border-gray-500 p-2 bg-slate-300 placeholder:text-gray-500 outline-neutral-950"
+            />
+            <button
+            type="submit"
+            className="w-16 rounded-e-md bg-slate-950 text-gray-400  hover:bg-slate-600 hover:text-gray-900"
+            >
+                Add</button>
+        </form>
+    )
+}

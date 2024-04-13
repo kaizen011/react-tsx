916f2bd160f10669c3e339efb4cb5b28f6d33764
Kaizen <125158784+kaizen011@users.noreply.github.com>
2024-04-13 15:54:31 +0200

    Prop drilling, sorted todos

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import TodoItem from "./components/TodoItem"
 import { dummyData } from "./data/todos"
 import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/ToDoList";
 
 function App() {
   const [todos, setTodos] = useState(dummyData);
@@ -27,19 +28,11 @@ function App() {
     <main className="py-8 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center text-slate-300">Your todos</h1>
       <div className="max-w-lg mx-auto bg-slate-900 rounded p-5 space-y-6">
-       <AddTodoForm 
-       onSubmit={addTodo}
-       />
-        <div className="space-y-2">
-          {todos.map(todo => (
-            <TodoItem
-            key={todo.id} 
-            todoExtract={todo}
-            onCompletedChange={setTodoCompleted}
-            id={todo.id}
-            />
-          ))}
-        </div>
+       <AddTodoForm onSubmit={addTodo}/>
+       <TodoList
+       todos={todos}
+       onCompletedChange={setTodoCompleted}/>
+       
       </div>
     </main>
   )
